42f60023141339e6c229b0168c90b5a2e63f7444
PranayShriwas <pranayshriwas420@gmail.com>
2023-06-10 19:52:22 +0530

    Built-in class-based generic views

--- company/company/urls.py
+++ company/company/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('emp.urls')),
-    path('/student', include('Student.urls'))
+    path('student/', include('Student.urls'))
 ]
